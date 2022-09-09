57894f1e53c5c8786356b406bc217a6b80ef8e49
magdalyndim <magdalyndim@gmail.com>
2022-09-08 21:41:38 -0700

    Write a C program that prints out something followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: always 1 (Success)
  */
 int main(void)
 {
-	      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-              return (1);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,59);
+	return (1);
 }
