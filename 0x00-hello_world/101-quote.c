060eec759cbc147450b2f021718f7842d33412cd
magdalyndim <magdalyndim@gmail.com>
2022-09-09 22:52:49 -0700

    Write a C program that prints followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
+ * Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
  * followed by a new line, to standard error.
  * Return: Always 1 (success)
  */
