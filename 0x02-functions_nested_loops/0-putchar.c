d6175750d83d957cf7eee51eea08f159d3cc2faf
magdalyndim <magdalyndim@gmail.com>
2022-09-14 06:15:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
-#include "main.h" 
-  
- /** 
-  * main - prints _putchar 
-  * 
-  * Description: prints _putchar 
-  * 
-  * Return: 0 (Success) 
-  */ 
-  
- int main(void) 
- { 
-         char text[9] = "_putchar"; 
-         int i = 0; 
-  
-         for (i = 0; i < 8; i++) 
-         { 
-                 _putchar(text[i]); 
-         } 
-         _putchar('\n'); 
-  
-         return (0); 
- }
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
