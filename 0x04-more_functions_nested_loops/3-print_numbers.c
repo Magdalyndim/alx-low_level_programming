7217a70d093aed9541e1cb4e25608ad332416c7d
magdalyndim <magdalyndim@gmail.com>
2022-09-15 16:44:09 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+
+	_putchar('\n');
+
+}	
