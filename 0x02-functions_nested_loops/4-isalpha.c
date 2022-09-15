c371977141e35d7dfe71e8331e17f4600692e6df
magdalyndim <magdalyndim@gmail.com>
2022-09-15 02:00:16 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include 4-isalpha.c "main.h"
+
+/**
+*  _isalpha - Entry point
+* @c: single letter input
+* Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+*/
+int _isalpha(int c)
+{
+
+if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+return (1);
+else
+return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,26 +7,12 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
-int _isupper(int c);
+int _islower(int c);
 
-int _isdigit(int c);
+int _isalpha(int c);
 
-int mul(int a, int b);
+int print_sign(int n);
 
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
-
-void print_number(int n);
+int _abs(int);
 
 #endif
