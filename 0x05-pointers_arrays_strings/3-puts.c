dadc18974e4eec6d49a9f6f70e205c482d24104d
magdalyndim <magdalyndim@gmail.com>
2022-09-19 12:06:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
