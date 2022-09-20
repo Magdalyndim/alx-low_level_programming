c3e11cdb49e8fc7a11fa7a7fbde03af3cd07b0da
magdalyndim <magdalyndim@gmail.com>
2022-09-19 21:00:41 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,25 +2,18 @@
 
 /**
  * print_rev - prints string backwards
- * @s: string
- * Return: void
+ * @s: string used as pointer reference
+ * Return: 0
  */
 
 void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
+	while (s[i])
 		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		_putchar(s[i]);
-		i--;
-	}
-	_putchar('\n');
 
-	return (void);
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
