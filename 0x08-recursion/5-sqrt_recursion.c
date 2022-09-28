2dd4a0940641867b09d0374426adc6fbc683bb1d
magdalyndim <magdalyndim@gmail.com>
2022-09-28 07:47:14 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - main funct
- * @n: int  n 
- * Return: int
+ * power_operation - returns the natural square root of a number
+ * @n: input number
+ * @c: iterator
+ * Return: square root or -1.
+ */
+
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == 0)
+			return (c);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: natural square root
  */
 
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-
-/**
- * _sqrt - _sqrt_recursion
- * @n: integer param
- * @i: integer param
- * Return: sqrt
- */
-
-int _sqrt(int n, int i)
-{
 	if (n < 0)
 		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
