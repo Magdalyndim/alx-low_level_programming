ee16aa9487e18f214b9d6fc112c8dd2ad380cfee
magdalyndim <magdalyndim@gmail.com>
2022-09-30 08:50:59 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,7 @@
  * main - adds positive numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * Return: 0 - success, 1 on failure
  */
 
 int main(int argc, char *argv[])
@@ -14,11 +14,11 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
-				printf("Eror\n");
+				printf("Error\n");
 				return (1);
 			}
 		}
