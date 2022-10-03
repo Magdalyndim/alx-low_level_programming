2cc2c93d94bc09ac202febb8698d7f20e7bddc8b
magdalyndim <magdalyndim@gmail.com>
2022-10-03 08:43:26 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
-		for (height >= 0; height--;)
+		for (; height >= 0; height--)
 			free(grid[height]);
 		free(grid);
 	}
