35d65d74444c3fa868bd16fe50cb771496577530
magdalyndim <magdalyndim@gmail.com>
2022-10-04 00:36:37 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,7 +35,7 @@ char **strtow(char *str)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != ' ' && (str[c + 1] == '\0'))
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
 	aout = malloc((height + 1) * sizeof(char *));
 	if (aout == NULL || height == 0)
