465c5b6f29a222c33e95845d76d33123da8d2a1e
Magdalyndim <magdalyndim@gmail.com>
2022-12-12 20:49:44 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/mag.h
@@ -0,0 +1,12 @@
+#ifndef _MAG_H_
+#define _MAG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+#endif
